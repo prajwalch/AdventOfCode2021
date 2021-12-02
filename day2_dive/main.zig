23c0323289d18d7fcf27e494e89bf4fbe5e8fff9
PrajwalCH <prajjwal2058@gmail.com>
2021-12-02 21:31:17 +0545

    refactor_day1: use more convenient method while parsing cmd from file

--- day2_dive/main.zig
+++ day2_dive/main.zig
@@ -12,19 +12,16 @@ pub fn main() anyerror!void {
 
     while (try reader.readUntilDelimiterOrEof(&cmd_buff, '\n')) |line| {
         var cmd_iter = std.mem.split(line, " ");
+        var cmd_name = cmd_iter.next().?;
+        var cmd_pos = try std.fmt.parseInt(usize, cmd_iter.rest(), 10);
 
-        if (cmd_iter.next()) |data| {
-            var cmd_name = data;
-            var cmd_pos = try std.fmt.parseInt(u8, cmd_iter.rest(), 10);
-
-            if (std.mem.eql(u8, cmd_name, "forward")) {
-                horizontal_pos += cmd_pos;
-                depth += aim * cmd_pos;
-            } else if (std.mem.eql(u8, cmd_name, "down")) {
-                aim += cmd_pos;
-            } else if (std.mem.eql(u8, cmd_name, "up")) {
-                aim -= cmd_pos;
-            }
+        if (std.mem.eql(u8, cmd_name, "forward")) {
+            horizontal_pos += cmd_pos;
+            depth += aim * cmd_pos;
+        } else if (std.mem.eql(u8, cmd_name, "down")) {
+            aim += cmd_pos;
+        } else if (std.mem.eql(u8, cmd_name, "up")) {
+            aim -= cmd_pos;
         }
     }
 
