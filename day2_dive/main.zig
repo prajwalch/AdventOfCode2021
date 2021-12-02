fb2583c895b163218e62f50ba05219a63c7b1b6e
PrajwalCH <prajjwal2058@gmail.com>
2021-12-02 13:42:44 +0545

    completed day2: Dive

---
+++ day2_dive/main.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+pub fn main() anyerror!void {
+    var horizontal_pos: usize = 0;
+    var depth: usize = 0;
+    var aim: usize = 0;
+
+    var file = try std.fs.cwd().openFile("input.txt", .{ .read = true });
+    defer file.close();
+    var reader = file.reader();
+    var cmd_buff: [8196]u8 = undefined;
+
+    while (try reader.readUntilDelimiterOrEof(&cmd_buff, '\n')) |line| {
+        var cmd_iter = std.mem.split(line, " ");
+
+        if (cmd_iter.next()) |data| {
+            var cmd_name = data;
+            var cmd_pos = try std.fmt.parseInt(u8, cmd_iter.rest(), 10);
+
+            if (std.mem.eql(u8, cmd_name, "forward")) {
+                horizontal_pos += cmd_pos;
+                depth += aim * cmd_pos;
+            } else if (std.mem.eql(u8, cmd_name, "down")) {
+                aim += cmd_pos;
+            } else if (std.mem.eql(u8, cmd_name, "up")) {
+                aim -= cmd_pos;
+            }
+        }
+    }
+
+    std.debug.print("{d}", .{horizontal_pos * depth});
+}
