fd69b77a0382ca86995a9239ddc01e501a14863c
PrajwalCH <prajjwal2058@gmail.com>
2021-12-03 09:09:06 +0545

    refactor_day2: separate code for both part 1 & 2

--- day2_dive/main.zig
+++ day2_dive/main.zig
@@ -1,14 +1,47 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
+    try partOne();
+    try partTwo();
+}
+
+fn partOne() !void {
+    var file = try std.fs.cwd().openFile("input.txt", .{ .read = true });
+    defer file.close();
+
+    var reader = file.reader();
+    var cmd_buff: [8196]u8 = undefined;
+
+    var horizontal_pos: usize = 0;
+    var depth: usize = 0;
+
+    while (try reader.readUntilDelimiterOrEof(&cmd_buff, '\n')) |line| {
+        var cmd_iter = std.mem.split(line, " ");
+        var cmd_name = cmd_iter.next().?;
+        var cmd_pos = try std.fmt.parseInt(usize, cmd_iter.rest(), 10);
+
+        if (std.mem.eql(u8, cmd_name, "forward")) {
+            horizontal_pos += cmd_pos;
+        } else if (std.mem.eql(u8, cmd_name, "down")) {
+            depth += cmd_pos;
+        } else if (std.mem.eql(u8, cmd_name, "up")) {
+            depth -= cmd_pos;
+        }
+    }
+
+    std.debug.print("Part One: {d}\n", .{horizontal_pos * depth});
+}
+
+fn partTwo() !void {
+    var file = try std.fs.cwd().openFile("input.txt", .{ .read = true });
+    defer file.close();
+
+    var reader = file.reader();
+    var cmd_buff: [8196]u8 = undefined;
+
     var horizontal_pos: usize = 0;
     var depth: usize = 0;
     var aim: usize = 0;
-
-    var file = try std.fs.cwd().openFile("input.txt", .{ .read = true });
-    defer file.close();
-    var reader = file.reader();
-    var cmd_buff: [8196]u8 = undefined;
 
     while (try reader.readUntilDelimiterOrEof(&cmd_buff, '\n')) |line| {
         var cmd_iter = std.mem.split(line, " ");
@@ -25,5 +58,5 @@ pub fn main() anyerror!void {
         }
     }
 
-    std.debug.print("{d}", .{horizontal_pos * depth});
+    std.debug.print("Part Two: {d}\n", .{horizontal_pos * depth});
 }
