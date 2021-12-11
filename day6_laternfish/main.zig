f4a9ab405d8e3998d49fdd17a3372d172a5230a9
PrajwalCH <prajjwal2058@gmail.com>
2021-12-11 12:08:08 +0545

    refactor day6 with part 2 addded

--- day6_laternfish/main.zig
+++ day6_laternfish/main.zig
@@ -4,33 +4,43 @@ const input = @embedFile("input.txt");
 const print = std.debug.print;
 
 pub fn main() anyerror!void {
-    try partOne(80);
+    var part_one = try simulateFishLifetime(80);
+    var part_two = try simulateFishLifetime(256);
+
+    print("Part One: {d}\n", .{part_one});
+    print("Part Two: {d}\n", .{part_two});
 }
 
-fn partOne(total_days: usize) !void {
-    var fish_ages = std.ArrayList(usize).init(std.heap.page_allocator);
-    defer fish_ages.deinit();
+fn simulateFishLifetime(total_days: usize) !u64 {
+    var lifetime_table = [_]u64{0} ** 9;
 
     var input_iter = std.mem.tokenize(input, ",");
     while (input_iter.next()) |raw_num| {
-        var parsed_num = try std.fmt.parseInt(usize, std.mem.trimRight(u8, raw_num, "\n"), 10);
-        try fish_ages.append(parsed_num);
+        var fish_life = try std.fmt.parseInt(u8, std.mem.trimRight(u8, raw_num, "\n"), 10);
+        lifetime_table[fish_life] += 1;
     }
 
+    var nums_resetting_fish: u64 = 0;
     var day: usize = 1;
-    while (day <= total_days) : (day += 1) {
-        //print("day {d}: ", .{day});
 
-        for (fish_ages.items) |*age, _| {
-            if (age.* == 0) {
-                age.* = 6;
-                try fish_ages.append(8);
+    while (day <= total_days) : (day += 1) {
+        for (lifetime_table) |fish_num, lifetime| {
+            if (lifetime == 0) {
+                nums_resetting_fish = fish_num;
             } else {
-                age.* -= 1;
+                lifetime_table[lifetime - 1] = fish_num;
             }
-            //print("{d},", .{age.*});
         }
-        //print("\n", .{});
+        lifetime_table[6] += nums_resetting_fish;
+        lifetime_table[8] = nums_resetting_fish;
     }
-    print("Part One: {d}\n", .{fish_ages.items.len});
+    return countTotalFish(lifetime_table);
+}
+
+fn countTotalFish(lifetime_table: [9]u64) u64 {
+    var count: u64 = 0;
+    for (lifetime_table) |nums_fish| {
+        count += nums_fish;
+    }
+    return count;
 }
